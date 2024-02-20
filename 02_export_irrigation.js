da50e970dab405fd97e12a0dea6480cc7795e084
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2024-02-20 11:45:59 -0300

    Update 02_export_irrigation.js

--- 02_export_irrigation.js
+++ 02_export_irrigation.js
@@ -1,1 +1,16 @@
+// export irrigation stats per municipality
+// dhemerson.costa@ipam.org.br
+
+// read irrigation layer
+var irrigation = ee.Image('');
+
+// read territories
+var territory = ee.FeatureCollection('users/dh-conciani/vectors/municipios_cerrado')
+  // create numerical ids
+  .map(function(feature) {
+    return (feature.set({'geocode': ee.Number.parse(feature.get('CD_MUN'))}));
+  });
+
+// rasterize
+territory = ee.Image().paint(territory, 'geocode').rename('territory');
 
